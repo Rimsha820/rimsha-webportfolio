ee906976855356e0c3b0afe591a2f2af5a70b7d2
Rimsha Malik <rimsha820.malik@gmail.com>
2025-05-14 11:42:34 +0500

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rimsha-webportfolio/', // 👈 IMPORTANT for GitHub Pages subfolder
   plugins: [react()],
 })
